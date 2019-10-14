0e9e504e8e586c95fd9e201511254b9fec90f413
Dimuth Kurukula <dimuthkurukula@gmail.com>
2019-10-14 16:10:44 -0400

    update time

--- setupubuntu.sh
+++ setupubuntu.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo apt-get remove -y --purge libreoffice*
+sudo apt-get remove -y --purge libreoffice* thunderbird ubuntu-web-launchers
 sudo apt-get clean -y
 sudo apt-get autoremove -y
 
@@ -10,3 +10,5 @@ sed -i "4s/$/$new/" $file
 
 source $file
 
+sudo timedatectl set-timezone US/Eastern
+gsettings set org.gnome.desktop.interface clock-format '12h'   
