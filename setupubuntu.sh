c112983ec1caa7b641f0dd429b7ddcef4a6ab930
Dimuth Kurukula <dimuthkurukula@gmail.com>
2019-11-13 08:07:08 -0500

    chage repos to universe, add pip and vim

--- setupubuntu.sh
+++ setupubuntu.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-sudo apt-get remove -y --purge libreoffice* thunderbird ubuntu-web-launchers
+sudo apt remove -y --purge libreoffice* thunderbird ubuntu-web-launchers tiny-vim
 sudo apt-get clean -y
 sudo apt-get autoremove -y
+
 
 new="set -o vi \n"
 file=~/.bashrc
@@ -12,3 +13,14 @@ source $file
 
 sudo timedatectl set-timezone US/Eastern
 gsettings set org.gnome.desktop.interface clock-format '12h'   
+
+file=/etc/apt/sources.list	
+sed -i "s/restricted/universe/g" $file
+
+sudo apt update
+sudo apt install -y software-properties-common apt-transport-https git curl pytho3-pip vim
+
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dpkg -i google-chrome-stable_current_amd64.deb
+rm google-chrome-stable_current_amd64.deb
+
