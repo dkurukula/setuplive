203d4134a34a486e4ad257b549d4a2b34304b7fe
Dimuth Kurukula <dimuthkurukula@gmail.com>
2019-11-13 19:54:37 -0500

    fix script

--- setupubuntu.sh
+++ setupubuntu.sh
@@ -8,17 +8,18 @@ sudo apt-get autoremove -y
 new="set -o vi \n"
 file=~/.bashrc
 sed -i "4s/$/$new/" $file
+source $file
 
+file=/etc/apt/sources.list	
+sed -i "s/restricted/universe/g" $file
 source $file
 
 sudo timedatectl set-timezone US/Eastern
 gsettings set org.gnome.desktop.interface clock-format '12h'   
 
-file=/etc/apt/sources.list	
-sed -i "s/restricted/universe/g" $file
 
 sudo apt update
-sudo apt install -y software-properties-common apt-transport-https git curl pytho3-pip vim
+sudo apt install -y software-properties-common apt-transport-https git curl python3-pip vim
 
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg -i google-chrome-stable_current_amd64.deb
