07a21017843c7a2d87cfe14a75423bac1e626eaa
Dimuth Kurukla <dimuthkurukula@gmail.com>
2019-11-18 08:24:56 -0500

    add zsh, add conditionals

--- setupubuntu.sh
+++ setupubuntu.sh
@@ -12,3 +12,25 @@ source $file
 
 sudo timedatectl set-timezone US/Eastern
 gsettings set org.gnome.desktop.interface clock-format '12h'   
+
+#update and upgrade only if not already done today
+[ -z "$(find -H /var/lib/apt/lists -maxdepth 0 -mtime -1)" ] && sudo apt update && apt upgrade
+
+
+sudo apt install -y zsh
+
+NEWDIR=~/.oh-my-zsh
+if [ ! -d "$NEWDIR" ]; then
+	git clone https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+	cp ~/.oh-my-zsh/templates/zshrc.zsh-template ~/.zshrc chsh -s /bin/zsh
+fi
+
+NEWDIR=$HOME/.zsh-syntax-highlighting
+if [ ! -d "$NEWDIR" ]; then
+	git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "$HOME/.zsh-syntax-highlighting" --depth 1
+fi
+
+NEWFILE=$HOME/.zshrc
+if [ ! -f "$NEWFILE" ]; then
+	echo "source $HOME/.zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> "$HOME/.zshrc"
+fi
